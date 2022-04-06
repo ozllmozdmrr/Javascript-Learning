a7907daedeb91febb476fc5da028544a1bb659c5
ozllmozdmrr <ozllmozdmrr00@gmail.com>
2022-04-06 14:35:21 +0300

    mouse events

---
+++ Dom-Document Object Module/mouse_event.js
@@ -0,0 +1,50 @@
+// Mouse events
+
+const btn = document.querySelector('#btnDeleteAll');
+const ul = document.querySelector('#task-list');
+
+
+// // click
+// btn.addEventListener('click',eventHandler)
+// ul.addEventListener('click',eventHandler);
+
+// function eventHandler(event){
+//     console.log(`event type : ${event.type}`);
+// }
+
+//  double click
+
+// btn.addEventListener('dblclick',eventHandler);
+
+// function eventHandler(event){
+//     console.log(`event type : ${event.type}`);
+// }
+
+// // mouse down
+// btn.addEventListener('mousedown',eventHandler);
+
+// // mouse up
+// btn.addEventListener('mouseup',eventHandler);
+
+// // mouse enter 
+// ul.addEventListener('mouseenter',eventHandler);
+
+// // mouse leave
+// ul.addEventListener('mouseleave',eventHandler);
+
+// // mouse over
+// ul.addEventListener('mouseover',eventHandler);
+
+// // mouse out
+// ul.addEventListener('mouseout',eventHandler);
+
+// mouse move
+ul.addEventListener('mousemove', eventHandler);
+
+
+
+
+
+function eventHandler(event){
+    console.log(`event type : ${event.type}`);
+}
