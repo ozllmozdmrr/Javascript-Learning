a6c7900dc04d2f45b0bfb50e270632477cacb8d5
Özlem Özdemir <97258275+ozllmozdmrr@users.noreply.github.com>
2022-04-08 11:44:08 +0300

    Update keyboard_event.js

--- Dom-Document Object Module/keyboard_event.js
+++ Dom-Document Object Module/keyboard_event.js
@@ -10,18 +10,18 @@ const select = document.querySelector('#select');
  input.addEventListener('keyup',eventHandler); // Anahtar serbest bırakıldığında keyup gerçekleşir.
  input.addEventListener('keypress',eventHandler); // Bu olay, bir alfabetik, sayısal veya noktalama tuşuna basıldığında tetiklenir.
  input.addEventListener('focus',eventHandler); // Mevcut pencereye odaklanır.
-// input.addEventListener('blur', eventHandler);  // Mevcut pencereden odağı kaldırır.
-// input.addEventListener('cut',eventHandler); //kopyala
-// input.addEventListener('paste',eventHandler);  // yapıştır.
-// input.addEventListener('select',eventHandler);
+ input.addEventListener('blur', eventHandler);  // Mevcut pencereden odağı kaldırır.
+ input.addEventListener('cut',eventHandler); //kopyala
+ input.addEventListener('paste',eventHandler);  // yapıştır.
+ input.addEventListener('select',eventHandler);
 
-// form.addEventListener('submit',eventHandler);
+ form.addEventListener('submit',eventHandler);
 
 select.addEventListener('change',eventHandler);
 
 function eventHandler(e){
     console.log('event type ' + e.type);
-    // console.log('key code:'+ e.keyCode);
+     // console.log('key code:'+ e.keyCode);
      console.log('value :'+ e.target.value);
 
     // e.target.style.backgroundColor='blue'
