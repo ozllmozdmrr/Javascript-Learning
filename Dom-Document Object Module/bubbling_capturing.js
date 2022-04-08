108923025f9134bb91eb4d3c284886d57c1fef1f
ozllmozdmrr <ozllmozdmrr00@gmail.com>
2022-04-08 17:29:39 +0300

    Bubbling & Capturing

---
+++ Dom-Document Object Module/bubbling_capturing.js
@@ -0,0 +1,78 @@
+const form = document.querySelector('form');
+const cardBody = document.querySelector('.card-body');
+const card = document.querySelector('.card');
+const container = document.querySelector('.container');
+
+
+// // Event Bubbling /*  içten dışa doğru   */
+// form.addEventListener('click',function(e){
+//     console.log('form');
+//     e.stopPropagation();
+// });
+
+// cardBody.addEventListener('click',function(e){
+//     console.log('card body');
+//     e.stopPropagation();
+// });
+
+// card.addEventListener('click',function(e){
+//     console.log('card');
+//     e.stopPropagation();
+
+// });
+
+// container.addEventListener('click',function(e){
+//     console.log('container');
+//     e.stopPropagation();
+// });
+
+
+
+
+// Event Capturing  // Dıştan içeri doğru 
+
+// form.addEventListener('click',function(e){
+//     console.log('form');
+//     e.stopPropagation();
+// },true);
+
+// cardBody.addEventListener('click',function(e){
+//     console.log('card body');
+//     e.stopPropagation();
+// },true);
+
+// card.addEventListener('click',function(e){
+//     console.log('card');
+//     e.stopPropagation();
+// },true);
+
+// container.addEventListener('click',function(e){
+//     console.log('container');
+//     e.stopPropagation();
+// },true);
+
+
+/*************** ÖRNEK *****************/
+
+// const deleteItems = document.querySelectorAll('.fa-times');
+
+// deleteItems.forEach(function(item){
+//     item.addEventListener('click',function(e){
+//         console.log(e.target);
+//     })
+// });
+
+/******** VEYA  ÜST ELEMANDAN ALT ELEMENA  *********/ 
+
+const ul = document.querySelector('ul');
+
+ul.addEventListener('click',function(e){  // ul 'ye event ekledik , ul den iç elemana geçiş yapma
+   
+   if(e.target.className==='fas fa-times'){
+    // console.log('icon');
+    // console.log(e.target);
+    console.log(e.target.parentElement);
+
+    e.preventDefault();
+   }
+});
--- Dom-Document Object Module/mouse_event.js
+++ Dom-Document Object Module/mouse_event.js
@@ -4,41 +4,6 @@ const btn = document.querySelector('#btnDeleteAll');
 const ul = document.querySelector('#task-list');
 
 
-<<<<<<< HEAD
-// click
-btn.addEventListener('click',eventHandler)
-ul.addEventListener('click',eventHandler);
-
-function eventHandler(event){
-    console.log(`event type : ${event.type}`);
-}
-
- //double click
-
-btn.addEventListener('dblclick',eventHandler);
-
-function eventHandler(event){
-    console.log(`event type : ${event.type}`);
-}
-
-// mouse down
-btn.addEventListener('mousedown',eventHandler);
-
-// mouse up
-btn.addEventListener('mouseup',eventHandler);
-
-// mouse enter 
-ul.addEventListener('mouseenter',eventHandler);
-
-// mouse leave
-ul.addEventListener('mouseleave',eventHandler);
-
-// mouse over
-ul.addEventListener('mouseover',eventHandler);
-
-// mouse out
-ul.addEventListener('mouseout',eventHandler);
-=======
 //  click
  btn.addEventListener('click',eventHandler)
  ul.addEventListener('click',eventHandler);
@@ -71,7 +36,6 @@ ul.addEventListener('mouseout',eventHandler);
 
 //  mouse out
  ul.addEventListener('mouseout',eventHandler);
->>>>>>> f0eaf8798e8f8857ddb21e8663fea49ff462a50c
 
 // mouse move
 ul.addEventListener('mousemove', eventHandler);
