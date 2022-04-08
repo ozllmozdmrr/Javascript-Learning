73e6e8d37295b2c5b16640e441b9b7a19bf4cc9c
ozllmozdmrr <ozllmozdmrr00@gmail.com>
2022-04-08 11:42:25 +0300

    Keyboard Event Updated

--- Dom-Document Object Module/keyboard_event.js
+++ Dom-Document Object Module/keyboard_event.js
@@ -2,14 +2,29 @@
 
 const input = document.querySelector('#txtTaskName');
 
-input.addEventListener('keydown',eventHandler);  // Tuşa basıldığında tuşa basma gerçekleşir ve tuşa uzun süre basıldığında otomatik olarak tekrarlanır.
-input.addEventListener('keyup',eventHandler); // Anahtar serbest bırakıldığında keyup gerçekleşir.
-input.addEventListener('keypress',eventHandler); // Bu olay, bir alfabetik, sayısal veya noktalama tuşuna basıldığında tetiklenir.
-input.addEventListener('focus',eventHandler);
+const form = document.querySelector('form');
 
+const select = document.querySelector('#select');
+
+// input.addEventListener('keydown',eventHandler);  // Tuşa basıldığında tuşa basma gerçekleşir ve tuşa uzun süre basıldığında otomatik olarak tekrarlanır.
+// input.addEventListener('keyup',eventHandler); // Anahtar serbest bırakıldığında keyup gerçekleşir.
+// input.addEventListener('keypress',eventHandler); // Bu olay, bir alfabetik, sayısal veya noktalama tuşuna basıldığında tetiklenir.
+// input.addEventListener('focus',eventHandler); // Mevcut pencereye odaklanır.
+// input.addEventListener('blur', eventHandler);  // Mevcut pencereden odağı kaldırır.
+// input.addEventListener('cut',eventHandler); //kopyala
+// input.addEventListener('paste',eventHandler);  // yapıştır.
+// input.addEventListener('select',eventHandler);
+
+// form.addEventListener('submit',eventHandler);
+
+select.addEventListener('change',eventHandler);
 
 function eventHandler(e){
-    console.log('event type' + e.type);
-    console.log('key code:'+ e.keyCode);
-    console.log('value :'+ e.target.value);
+    console.log('event type ' + e.type);
+    // console.log('key code:'+ e.keyCode);
+     console.log('value :'+ e.target.value);
+
+    // e.target.style.backgroundColor='blue'
+
+    e.preventDefault();
 }
